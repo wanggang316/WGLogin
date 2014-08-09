912db76c1c7f1cfc816764d00cff72fc1247bec2
wanggang <wgang316@163.com>
2014-08-09 12:04:51 +0800

    Initial Commit

---
+++ WGLogin/WGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGAppDelegate.h
+//  WGLogin
+//
+//  Created by 王刚 on 14/8/9.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WGLoginTests/WGLoginTests.m
@@ -0,0 +1,34 @@
+//
+//  WGLoginTests.m
+//  WGLoginTests
+//
+//  Created by 王刚 on 14/8/9.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WGLoginTests : XCTestCase
+
+@end
+
+@implementation WGLoginTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
